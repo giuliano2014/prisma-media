a10250c12c2a8646b0e62a224d9efb485d6f67c4
Giuliano <giulianofurgol@hotmail.com>
2018-04-24 11:08:36 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1 className="App-title">It works !</h1>
+      <h1>It works !</h1>
     );
   }
 }
